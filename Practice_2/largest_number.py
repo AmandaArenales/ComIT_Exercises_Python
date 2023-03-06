1e633f7315512e40b9c18d946f277bbb3de038ca
Amanda Arenales <amandaarenales@gmail.com>
2023-03-06 11:18:41 -0700

    Added more exercises on Practice_2

--- Practice_2/largest_number.py
+++ Practice_2/largest_number.py
@@ -4,20 +4,19 @@ numbers that are read from the keyboard. The user ends by entering -1."""
 def largest_number():
     number = 0
     number  = float(input("Please, insert the first number: "))
+    largest = number
     list_numbers = []
     list_numbers.append(number)
 
-    
     while number != -1:
         new_number  = float(input("Please, insert a number or -1 if you don't want yo enter other number: "))
         
-        for n in list_numbers:
-            if (new_number > number):
-                largest = new_number
+        if (new_number > largest):
+            largest = new_number
         
         number = new_number
         list_numbers.append(number)
 
-    print (f"The largest number between {list_numbers} is {largest}")
+    print (f"The largest number in {list_numbers} is {largest}")
 
 largest_number()
