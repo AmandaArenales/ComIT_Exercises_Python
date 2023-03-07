155bbe27cf7741340455c808529312c338a8b307
Amanda Arenales <amandaarenales@gmail.com>
2023-03-07 15:21:30 -0700

    Added more exercises on Practice_3

--- Practice_3/invert_a_string.py
+++ Practice_3/invert_a_string.py
@@ -1,18 +1,25 @@
 """Write an algorithm to invert a string of characters."""
 
-string = input("Please, insert a string: ")
-invert_string = ""
+def invert_string():
+    string = input("Please, insert a string: ")
+    invert_string = ""
 
-for letter in string:
-    invert_string = letter + invert_string
+    for letter in string:
+        invert_string = letter + invert_string
 
-print(f"The inverse of {string} is {invert_string}!")
+    print(f"The inverse of {string} is {invert_string}!")
+invert_string()
 
 #Or
-invert_string = ""
 
-for letter in range(1, len(string) + 1):
-    invert_string = invert_string + string[-1 * letter]
+def invert_string_1():
+    string = input("Please, insert a string: ")
+    invert_string = ""
 
-print(f"The inverse of {string} is {invert_string}!")
+    for letter in range(1, len(string) + 1):
+        invert_string = invert_string + string[-1 * letter]
+
+    print(f"The inverse of {string} is {invert_string}!")
+
+invert_string_1()
     
