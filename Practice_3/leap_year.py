d11f65a5f8d077b3f6c34d3c117c7274b3aec582
Amanda Arenales <amandaarenales@gmail.com>
2023-03-07 09:33:29 -0700

    Added more exercises on Practice_3

---
+++ Practice_3/leap_year.py
@@ -0,0 +1,14 @@
+"""Write a sub-algorithm that has a parameter 
+of type number that represents a year and determines whether the year is leap year or not.""" 
+
+def leap_year(year):
+
+    if (year % 4) == 0:
+        print(f"The {year} is a leap year!")
+    else:
+        print(f"The {year} is not a leap year!")
+
+
+year = int(input("Please, insert a year: "))
+leap_year(year)
+
