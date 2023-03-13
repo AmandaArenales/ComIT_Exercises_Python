2b2b89d2b5b0afd04e8e8673f54bdbda7e4617e0
Amanda Arenales <amandaarenales@gmail.com>
2023-03-13 13:24:27 -0600

    Add more exercises

--- P5_Python/position.py
+++ P5_Python/position.py
@@ -10,18 +10,9 @@ by two integer values ​​x and y. Available operations are:
 class Position:
     position_x= 0
     position_y = 0
-    incX = 0
-    incY = 0
-    decX = 0
-    decY = 0
 
     def __init__(self, *args):
-        if len(args) == 0:
-            pass
-        elif (len(args) == 2):
-            self.position_x = args[0]
-            self.position_y = args[1]
-        elif (len(args) == 4):
+        if (len(args) == 2):
             self.position_x = args[0]
             self.position_y = args[1]
     
@@ -30,8 +21,8 @@ class Position:
         self.position_y = position_y
         print(f"The position of the coordinate (x, y) is: ({position_x, position_y})")
 
-    def get_position_x_y(self, position_x, position_y):
-        return (position_x, position_y)
+    def get_position_x_y(self):
+        return (self.position_x, self.position_y)
     
     def increase_value(self, incX, incY):
         incX = self.position_x + incX
@@ -43,18 +34,33 @@ class Position:
         decY = self.position_y - decY
         print(f"The value of the new the coordinate (x, y) after decrement it is: ({decX, decY})")
 
-x = int(input("Please, insert the value for the position x: "))
-y = int(input("Please, insert the value for the position y: "))
+x = int(input("P1 - insert the value for x: "))
+y = int(input("P1 - insert the value for y: "))
 
-p = Position()
-p.set_setXY(x, y)
+p1 = Position()
+p1.set_setXY(x, y)
 
 inc_x =  int(input("Please, insert the increasing value for the position x: "))
 inc_y =  int(input("Please, insert the increasing value for the position y: "))
 
-p.increase_value(inc_x, inc_y)
+p1.increase_value(inc_x, inc_y)
 
 dec_x =  int(input("Please, insert the decreasing value for the position x: "))
 dec_y =  int(input("Please, insert the decreasing value for the position y: "))
 
-p.decrease_value(dec_x, dec_y)
+p1.decrease_value(dec_x, dec_y)
+
+x = int(input("P2 - insert the value for x: "))
+y = int(input("P2 - insert the value for y: "))
+
+p2 = Position(x, y)
+
+inc_x =  int(input("Please, insert the increasing value for the position x: "))
+inc_y =  int(input("Please, insert the increasing value for the position y: "))
+
+p2.increase_value(inc_x, inc_y)
+
+dec_x =  int(input("Please, insert the decreasing value for the position x: "))
+dec_y =  int(input("Please, insert the decreasing value for the position y: "))
+
+p2.decrease_value(dec_x, dec_y)
